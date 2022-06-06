f5121f954f185fff36d69d010abd31546a65a424
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-06-06 15:23:22 +0200

    Added Footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Footer = () => {
+  return (
+      <Wrapper>
+          <a href="https://github.com/tomaszburas/Cars-Ads" title="github" target="_blank">@Tomaszenko</a>
+      </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  position: absolute;
+  height: 1.6rem;
+  bottom: 0;
+  background-color: ${(props) => props.theme.colors.blue};
+  width: 100%;
+  color: ${(props) => props.theme.colors.white};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: .8rem;
+`
